b189ebd28da263a90d8d7315f3fa4e9003d9ad49
Simon Walker <s.r.walker101@googlemail.com>
2014-03-24 12:06:43 +0000

    Adds log wrapper class
    this handles enabling and disabling debug logging

--- ngts_catalogue/ngts_logging.py
+++ ngts_catalogue/ngts_logging.py
@@ -1,4 +1,29 @@
 import logging
+from functools import partial
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('ngts_catalogue')
+
+class NGTSLogger(object):
+    def __init__(self):
+        self.logger = logging.getLogger('ngts_catalogue')
+        self.log_level = self.logger.getEffectiveLevel()
+
+        level_map = {
+                'debug': logging.DEBUG,
+                'info': logging.INFO,
+                'warning': logging.WARNING,
+                'error': logging.ERROR,
+                'critical': logging.CRITICAL
+                }
+
+        for level_name, level in level_map.iteritems():
+            fn = partial(self.logger.log, level)
+            setattr(self, level_name.lower(), fn)
+
+    def enable_debug(self):
+        self.logger.setLevel(logging.DEBUG)
+
+    def disable_debug(self):
+        self.logger.setLevel(self.log_level)
+
+logger = NGTSLogger()
