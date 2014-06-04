972d515b13dd2d9b1cb48db5e245f9c634e61d09
Simon Walker <s.r.walker101@googlemail.com>
2014-06-04 15:55:36 +0100

    Making job queue fixture

--- testing/test_submit_job.py
+++ testing/test_submit_job.py
@@ -1,25 +1,40 @@
 import mock
 from ngts_zlp.submit_job import JobQueue
+import pytest
 
-def test_qsub():
+@pytest.fixture
+def job_queue():
+    return JobQueue()
+
+def test_qsub(job_queue):
     job_name = 'test_job_name'
-    assert JobQueue().qsub(job_name) == [
+    assert job_queue.qsub(job_name) == [
             'qsub', '-cwd', '-N', job_name,
                 '-S', '/bin/bash',
                 '-q', 'parallel']
 
-
-def test_add_job_calls_qsub():
-    jq = JobQueue()
-
+def test_add_job_calls_qsub(job_queue):
     test_job = ['ls', ]
     job_name = 'testing-ls'
 
-    with mock.patch.object(jq, 'qsub') as mock_qsub:
+    with mock.patch.object(job_queue, 'qsub') as mock_qsub:
         mock_qsub.return_value = ['qsub_stub', '-cwd', '-N', job_name,
                 '-S', '/bin/bash',
                 '-q', 'parallel']
 
-        value = jq.add_job(test_job, job_name)
+        job_queue.add_job(test_job, job_name)
 
-        assert mock_qsub.called_once() and value == 'ls\n'
+        assert mock_qsub.called_once()
+
+def test_submit_ls_passes_ls(job_queue):
+    test_job = ['ls', ]
+    job_name = 'testing-ls'
+
+    with mock.patch.object(job_queue, 'qsub') as mock_qsub:
+        mock_qsub.return_value = ['qsub_stub', '-cwd', '-N', job_name,
+                '-S', '/bin/bash',
+                '-q', 'parallel']
+
+        value = job_queue.add_job(test_job, job_name)
+        assert value == 'ls\n'
+
