9bb4ad07e1dc7fd586fc1080d4c237b6c813c2f5
Simon Walker <s.r.walker101@googlemail.com>
2014-06-04 17:07:35 +0100

    Adds test for job submission

--- ngts_zlp/tasks.py
+++ ngts_zlp/tasks.py
@@ -3,14 +3,22 @@ class Task(object):
         self.job_runner = job_runner
         self.jobname = jobname
 
+    def submit(self, *args, **kwargs):
+        job_command = self.job_command(*args, **kwargs)
+        return self.job_runner.add_job(job_command, self.jobname,
+                synchronous=self.synchronous)
+
+
 class CreateInputListTask(Task):
     script_name = 'create_lists.py'
+    synchronous = True
 
     def job_command(self, image_dirs):
         return [self.script_name, image_dirs, 'IMAGE', 'fits', self.jobname]
 
 class CreateMasterBiasTask(Task):
     script_name = 'pipebias.py'
+    synchronous = True
 
     def output_file(self):
         return '{jobname}_MasterBias.fits'.format(jobname=self.jobname)
--- testing/test_create_master_bias.py
+++ testing/test_create_master_bias.py
@@ -1,4 +1,5 @@
 from ngts_zlp.tasks import CreateMasterBiasTask
+from ngts_zlp.submit_job import JobQueue
 
 def test_build_master_bias_command():
     job_runner = 0
@@ -11,4 +12,15 @@ def test_build_master_bias_command():
     assert t.job_command(bias_list, output_dir) == [
             'pipebias.py', bias_list, 'test_job_MasterBias.fits',
             output_dir]
+
+def test_submit_master_bias_job():
+    job_runner = JobQueue(qsub_command='qsub_stub')
+    jobname = 'test_job'
+    output_dir = '/output'
+    bias_list = 'biaslist.txt'
+
+    t = CreateMasterBiasTask(job_runner, jobname)
+
+    result = t.submit(bias_list, output_dir)
+    assert result == 'pipebias.py biaslist.txt test_job_MasterBias.fits /output'
 
