dfa82fc8a5846f6ac41e722d4ea533500f3ce8ce
Simon Walker <s.r.walker101@googlemail.com>
2014-06-04 16:13:55 +0100

    Adds synchronous argument

--- ngts_zlp/submit_job.py
+++ ngts_zlp/submit_job.py
@@ -1,10 +1,14 @@
 import subprocess as sp
 
 class JobQueue(object):
-    def qsub(self, jobname, queue='parallel'):
-        return ['qsub', '-cwd', '-N', jobname,
+    def qsub(self, jobname, queue='parallel', synchronous=False):
+        cmd = ['qsub', '-cwd', '-N', jobname,
                 '-S', '/bin/bash',
                 '-q', queue]
+        if synchronous:
+            cmd += ['-sync', 'y']
+
+        return cmd
 
     def add_job(self, cmd, jobname):
         p = sp.Popen(self.qsub(jobname), stdout=sp.PIPE, stdin=sp.PIPE)
--- testing/test_submit_job.py
+++ testing/test_submit_job.py
@@ -21,6 +21,15 @@ def test_qsub(job_queue):
                 '-S', '/bin/bash',
                 '-q', 'parallel']
 
+def test_qsub_syncronous(job_queue):
+    job_name = 'test_job_name'
+    assert job_queue.qsub(job_name, synchronous=True) == [
+            'qsub', '-cwd', '-N', job_name,
+                '-S', '/bin/bash',
+                '-q', 'parallel',
+                '-sync', 'y']
+
+
 def test_add_job_calls_qsub(mock_qsub, job_queue):
     job_queue.add_job(['ls', ], 'mock_job')
     assert mock_qsub.called_once()
