ab5e42a1a2255d061b4a2ec8bec5a25980129ef3
Simon Walker <s.r.walker101@googlemail.com>
2014-06-05 12:39:07 +0100

    Adds test for submitting job

--- testing/test_input_list_task.py
+++ testing/test_input_list_task.py
@@ -10,3 +10,12 @@ def test_build_input_list_command():
     assert t.job_command(image_dirs) == ['createlists.py',
             image_dirs, 'IMAGE', 'fits', jobname]
 
+def test_submit_input_list_job(job_queue):
+    jobname = 'test_job'
+    image_dirs = 'imagedirs'
+
+    t = CreateInputListTask(job_queue, jobname)
+
+    result = t.submit(image_dirs)
+    assert result == 'createlists.py imagedirs IMAGE fits test_job'
+
