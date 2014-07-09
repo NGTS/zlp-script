a924a2e09853ca73a755483052ec8af36f9ef055
Simon Walker <s.r.walker101@googlemail.com>
2014-07-09 15:47:27 +0100

    Ensures reduction works

--- ZLP_TestRun1_srw.sh
+++ ZLP_TestRun1_srw.sh
@@ -4,9 +4,16 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-readonly WORKINGDIR=/ngts/pipedev
-readonly IMGDIRS=${WORKINGDIR}/OriginalData/images/201307[0-1][1,4,5]/*
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <root_directory>" >&2
+    exit 1
+fi
+
+readonly BASEDIR=$(readlink -f $(dirname $0))
+readonly WORKINGDIR=$(readlink -f ${1})
+readonly IMGDIRS=${WORKINGDIR}/OriginalData/images/**/*
 readonly RUNNAME="SimonTest1"
+readonly SCRIPTDIR=${BASEDIR}/scripts
 
 readonly BIASLIST=${WORKINGDIR}/OriginalData/output/${RUNNAME}_bias.list
 readonly DARKLIST=${WORKINGDIR}/OriginalData/output/${RUNNAME}_dark.list
@@ -16,13 +23,13 @@ readonly CONFMAP=${WORKINGDIR}/InputCatalogue/srw_confidence.fits
 readonly CORES=12
 
 
-readonly T1="0" # create input lists
-readonly T2="0" # create masterbias
-readonly T3="0" # create masterdark
-readonly T4="0" # copy temporary shutter map
-readonly T5="0" # create masterflat
+readonly T1="1" # create input lists
+readonly T2="1" # create masterbias
+readonly T3="1" # create masterdark
+readonly T4="1" # copy temporary shutter map
+readonly T5="1" # create masterflat
 readonly T6="0" # reduce dithered images
-readonly T7="0" # reduce science images
+readonly T7="1" # reduce science images
 readonly T8="0" # wait for jobs to finish
 readonly T9="0" # create input catalogues
 readonly T10="0" # perform photometry
@@ -38,30 +45,30 @@ readonly T13="0" # detrend
 
 create_input_lists() {
     echo "Create lists with Images"
-    CMD="${WORKINGDIR}/OriginalData/scripts/createlists.py \"$IMGDIRS\" IMAGE fits $RUNNAME"
+    CMD="${SCRIPTDIR}/createlists.py \"$IMGDIRS\" IMAGE fits $RUNNAME"
     echo $CMD
-    submit_synchronous_job "${CMD}" "${RUNNAME}_IMAGE_LIST"
+    ${CMD}
 }
 
 create_master_bias() {
     # Create MasterBias
     echo "Create MasterBias"
-    CMD="`system_python` /home/ag367/progs/pipebias.py $BIASLIST ${RUNNAME}_MasterBias.fits ${WORKINGDIR}/Reduction/${RUNNAME}"
-    submit_synchronous_job "${CMD}" "${RUNNAME}_BIAS"
+    CMD="`system_python` ${SCRIPTDIR}/pipebias.py $BIASLIST ${RUNNAME}_MasterBias.fits ${WORKINGDIR}/Reduction/output/${RUNNAME}"
+    ${CMD}
 }
 
 create_master_dark() {
     #Create MasterDark
     echo "Create MasterDark"
-    CMD="`system_python` /home/ag367/progs/pipedark.py $DARKLIST ${RUNNAME}_MasterBias.fits ${RUNNAME}_MasterDark.fits ${WORKINGDIR}/Reduction/${RUNNAME}"
-    submit_synchronous_job "${CMD}" "${RUNNAME}_DARK"
+    CMD="`system_python` ${SCRIPTDIR}/pipedark.py $DARKLIST ${RUNNAME}_MasterBias.fits ${RUNNAME}_MasterDark.fits ${WORKINGDIR}/Reduction/output/${RUNNAME}"
+    ${CMD}
 }
 
 copy_temporary_shuttermap() {
     echo "Copying temporary shutter map"
     # # !!!! be carefull, shuttermap not yet automatically created. will be copied manualy
     # #
-    DEST=${WORKINGDIR}/Reduction/${RUNNAME}
+    DEST=${WORKINGDIR}/Reduction/output/${RUNNAME}
     ensure_directory "${DEST}"
     cp /home/ag367/test/shuttermap.fits "${DEST}/${SHUTTERMAP}"
 }
@@ -70,8 +77,8 @@ copy_temporary_shuttermap() {
 create_master_flat() {
     #Create MasterFlat
     echo "Create MasterFlat"
-    CMD="`system_python` /home/ag367/progs/pipeflat.py $FLATLIST ${RUNNAME}_MasterBias.fits ${RUNNAME}_MasterDark.fits $SHUTTERMAP ${RUNNAME}_MasterFlat.fits ${WORKINGDIR}/Reduction/${RUNNAME}"
-    submit_synchronous_job "${CMD}" "${RUNNAME}_FLAT"
+    CMD="`system_python` ${SCRIPTDIR}/pipeflat.py $FLATLIST ${RUNNAME}_MasterBias.fits ${RUNNAME}_MasterDark.fits $SHUTTERMAP ${RUNNAME}_MasterFlat.fits ${WORKINGDIR}/Reduction/output/${RUNNAME}"
+    ${CMD}
 }
 
 reduce_images() {
@@ -79,20 +86,15 @@ reduce_images() {
     # Function submits jobs asynchronously and returns the list of job names
     #   used to run the analysis so the wait step can halt other processing.
     IMAGELISTS="${1}"
-    JOBNAMES=""
     counter="0"
     for IMAGELIST in ${IMAGELISTS}
     do 
         IMAGELIST=${IMAGELIST#${WORKINGDIR}} 
         IMAGELIST=${IMAGELIST#/OriginalData/output/} 
-        ensure_directory ${WORKINGDIR}/Reduction/${RUNNAME}/${IMAGELIST%.*}
-        CMD="`system_python` /home/ag367/progs/pipered.py ${WORKINGDIR}/OriginalData/output/$IMAGELIST ${RUNNAME}_MasterBias.fits ${RUNNAME}_MasterDark.fits $SHUTTERMAP ${RUNNAME}_MasterFlat.fits ${WORKINGDIR}/Reduction/${RUNNAME} ${WORKINGDIR}/Reduction/${RUNNAME}/${IMAGELIST%.*}"
-        submit_asynchronous_job "${CMD}" "${IMAGELIST%.*}"
-        if [ "$counter" -ne "0" ] ; then JOBNAMES=${JOBNAMES}"," ; fi 
-        JOBNAMES=${JOBNAMES}${IMAGELIST%.*}
-        counter="1"
+        ensure_directory ${WORKINGDIR}/Reduction/output/${RUNNAME}/${IMAGELIST%.*}
+        CMD="`system_python` ${SCRIPTDIR}/pipered.py ${WORKINGDIR}/OriginalData/output/$IMAGELIST ${RUNNAME}_MasterBias.fits ${RUNNAME}_MasterDark.fits $SHUTTERMAP ${RUNNAME}_MasterFlat.fits ${WORKINGDIR}/Reduction/output/${RUNNAME} ${WORKINGDIR}/Reduction/output/${RUNNAME}/${IMAGELIST%.*}"
+        ${CMD}
     done
-    echo $JOBNAMES
 }
 
 reduce_dithered_images() {
@@ -168,6 +170,7 @@ perform_aperture_photometry() {
             # find ${WORKINGDIR}/Reduction/${RUNNAME}/${JOBNAME} -name '*.fits' | head -n 100 > ${IMAGEFILELIST}
 
             find ${WORKINGDIR}/Reduction/${RUNNAME}/${JOBNAME} -name '*.fits' > ${IMAGEFILELIST}
+            # TODO: Needs CATPATH
             echo "${WORKINGDIR}/AperturePhot/run_app_photom.sh ${IMAGEFILELIST} ${CONFMAP} ${CATFILE} ${OUTPUTDIR}"
             qsub -N ${JOBNAME} -S /bin/bash -cwd ${WORKINGDIR}/AperturePhot/run_app_photom.sh ${IMAGEFILELIST} ${CONFMAP} ${CATFILE} ${OUTPUTDIR}
 
@@ -234,34 +237,31 @@ ensure_directory() {
     test -d ${DIR} || mkdir -p ${DIR}
 }
 
-submit_synchronous_job() {
-    CMD="${1}"
-    JOBNAME="${2}"
-    echo "${CMD}" | qsub -cwd -N "${JOBNAME}" -S /bin/bash -sync y -q parallel >/dev/null
-}
-
-submit_asynchronous_job() {
-    CMD="${1}"
-    JOBNAME="${2}"
-    echo "${CMD}" | qsub -N "${JOBNAME}" -S /bin/bash -q parallel >/dev/null
-}
-
 setup_environment() {
     echo "Environment set up"
     # BASEPATH=/ngts/pipedev/InputCatalogue
     # source ${BASEPATH}/venv/bin/activate
     # export PATH=/usr/local/casutools/bin:${PATH}
     # export LD_LIBRARY_PATH=/usr/local/wcslib/lib:/usr/local/cfitsio/lib:${LD_LIBRARY_PATH}
+
 }
 
 system_python() {
     echo "/usr/local/python/bin/python"
 }
 
+setup_directory_structure() {
+    for subdir in OriginalData/output AperturePhot Reduction Reduction/output; do
+        local dirpath=${WORKINGDIR}/${subdir}
+        ensure_directory ${WORKINGDIR}/${subdir}
+    done
+}
+
 # Do photometry on subtracted Images
 
 main() {
     setup_environment
+    setup_directory_structure
 
     cd ${WORKINGDIR}/OriginalData 
     [ "$T1" = "1" ] && create_input_lists
@@ -289,4 +289,4 @@ main() {
     [ "$T13" = "1" ] && run_detrending 
 }
 
-main
+main 2>&1 | tee zlp.log
