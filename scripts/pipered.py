64d724e4c9b2407d538f5b613f9afe43ac9f3b5a
Simon Walker <s.r.walker101@googlemail.com>
2014-10-27 09:37:56 +0000

    Remove shuttermap correction

--- scripts/pipered.py
+++ scripts/pipered.py
@@ -9,13 +9,11 @@ from extract_overscan import extract_overscan
 inlist = str(sys.argv[1])
 biasname = str(sys.argv[2])
 darkname = str(sys.argv[3])
-smname = str(sys.argv[4])
 flatname = str(sys.argv[5])
 caldir = str(sys.argv[6])+'/'
 outdir = str(sys.argv[7])+'/'
 biasname = caldir+biasname
 darkname = caldir+darkname
-smname = caldir+os.path.basename(smname)
 flatname = caldir+flatname
 
 os.system('rm -f '+outdir+'processed.dat')
@@ -29,8 +27,6 @@ def reducer():
     dark = hdulist[0].data
     hdulist = pyfits.open(flatname)
     flat = hdulist[0].data    
-    hdulist = pyfits.open(smname)
-    sm = hdulist[0].data    
     
     i = 1
     
@@ -40,7 +36,7 @@ def reducer():
         overscan = extract_overscan(hdulist)
         data = hdulist[0].data[0:2048,20:2068]
         exposure = hdulist[0].header['exposure']
-        corrected = ((data-np.median(overscan)-bias-(dark*exposure))/(1-sm/exposure))/flat
+        corrected = (data-np.median(overscan)-bias-(dark*exposure))/flat
         path, fname = os.path.split(stripped)
         outname = outdir+'proc'+fname
         print outname
@@ -48,7 +44,6 @@ def reducer():
         hdulist[0].header.add_history('Overscan of '+str(np.median(overscan))+' subtracted')
         hdulist[0].header.add_history('Bias subtracted using '+str(biasname))
         hdulist[0].header.add_history('Dark subtracted using '+str(darkname))
-        hdulist[0].header.add_history('Shuttercorrected using '+str(smname))
         hdulist[0].header.add_history('Flat corrected using '+str(flatname))
         
         command = 'rm -f '+outname
