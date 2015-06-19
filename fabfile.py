9e4cbb848630babf9f8a7555a6e4f2906261ab00
Simon Walker <s.r.walker101@googlemail.com>
2015-06-19 11:17:57 +0100

    Add fab command to run tests on remote

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,9 @@ from fabric.api import *
 env.use_ssh_config = True
 env.hosts = ['ngtshead.astro']
 
+PIPELINE_DIR = '~/work/NGTS/pipeline/ZLP/zlp-script'
+change_to_pipeline_dir = lambda: cd(PIPELINE_DIR)
+
 
 @task(default=True)
 def update():
@@ -17,8 +20,13 @@ def push_local(remote='origin', branch='master'):
 
 @task
 def update_remote(remote='origin', branch='master'):
-    with cd('~/work/NGTS/pipeline/ZLP/zlp-script'):
+    with change_to_pipeline_dir():
         run('git fetch {remote}'.format(remote=remote))
         run('git checkout {branch}'.format(branch=branch))
         run('git merge --ff {remote}/{branch}'.format(remote=remote, branch=branch))
         run('git submodule update')
+
+@task
+def test_remote(sourcedir='source2015'):
+    with change_to_pipeline_dir():
+        run('./test.sh {sourcedir}'.format(sourcedir=sourcedir))
