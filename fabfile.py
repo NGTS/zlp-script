b866f168d1963cc6ce212ed99649bf25b7697d5f
Simon Walker <s.r.walker101@googlemail.com>
2015-06-25 11:56:30 +0100

    Update fabfile tasks
    Remove default task from fabfile
    
    Add documentation for fab tasks
    
    Add function to extract submodules
    
    Add task to update all submodules

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 from fabric.api import *
+import os
 
 env.use_ssh_config = True
 env.hosts = ['ngtshead.astro']
@@ -7,19 +8,28 @@ PIPELINE_DIR = '~/work/NGTS/pipeline/ZLP/zlp-script'
 change_to_pipeline_dir = lambda: cd(PIPELINE_DIR)
 
 
-@task(default=True)
+@task
 def update():
+    '''
+    Push current changes and update on ngtshead
+    '''
     push_local()
     update_remote()
 
 
 @task
 def push_local(remote='origin', branch='master'):
+    '''
+    Push <remote>/<branch> to remote
+    '''
     local('git push {remote} {branch}'.format(remote=remote, branch=branch))
 
 
 @task
 def update_remote(remote='origin', branch='master'):
+    '''
+    Checkout <remote>/<branch> on ngtshead and update submodules
+    '''
     with change_to_pipeline_dir():
         run('git fetch {remote}'.format(remote=remote))
         run('git checkout {branch}'.format(branch=branch))
@@ -28,5 +38,28 @@ def update_remote(remote='origin', branch='master'):
 
 @task
 def test_remote(sourcedir='source2015'):
+    '''
+    Run test on ngtshead with <sourcedir>
+    '''
     with change_to_pipeline_dir():
         run('./test.sh {sourcedir}'.format(sourcedir=sourcedir))
+
+def submodules(fname):
+    root_dir = os.path.realpath(
+            os.path.dirname(fname))
+    with open(fname) as infile:
+        for line in infile:
+            if 'path' in line:
+                stub = line.strip().split('=')[-1].strip()
+                yield os.path.join(root_dir, stub)
+
+
+@task
+def update_submodules():
+    '''
+    Update all submodules to origin/master
+    '''
+    for path in submodules('.gitmodules'):
+        with lcd(path):
+            local('git fetch origin')
+            local('git checkout origin/master')
